be6c6de2058febf62d1484a0f16b1dc5f1064a1f
yury-yury <yury_yury@mail.ru>
2023-03-27 01:03:57 +0300

    fix_coverege

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -7,7 +7,12 @@ class TestArrs(unittest.TestCase):
     def test_get(self):
         self.assertEqual(arrs.get([1, 2, 3], 1, "test"), 2)
         self.assertEqual(arrs.get([], 0, "test"), "test")
+        self.assertEqual(arrs.get([1], -1), None)
+
 
     def test_slice(self):
         self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
         self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
+        self.assertEqual(arrs.my_slice([]), [])
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4], -5, 3), [1, 2, 3])
+        self.assertEqual(arrs.my_slice([1, 2, 3, 4], -3, 3), [2, 3])
